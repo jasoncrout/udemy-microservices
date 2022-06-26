139f9aa8057fb01fc091e710dc5ee27ad9ff00d0
Jason Crout <croutjason@gmail.com>
2022-06-27 09:02:40 +1000

    feat: fetch-json project

--- 3-typescript/fetch-json/index.ts
+++ 3-typescript/fetch-json/index.ts
@@ -2,9 +2,32 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const logTodo = (
+  id: Todo["id"],
+  title: Todo["title"],
+  completed: Todo["completed"],
+) => {
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of: ${title}
+  Is it finished?: ${completed}
+  `);
+};
+
 const request = async () => {
-  const data = await axios.get(url);
-  console.log(data);
+  const response = await axios.get(url);
+  const todo = response.data as Todo;
+
+  const { id, title, completed } = todo;
+
+  console.log(todo);
+  logTodo(id, title, completed);
 };
 
 request();
