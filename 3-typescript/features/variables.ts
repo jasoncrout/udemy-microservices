9550c25d5659b90636c03e0218787d22c8656e0f
Jason Crout <croutjason@gmail.com>
2022-06-29 15:50:12 +0930

    feat: add objects and functions code snippets

--- 3-typescript/features/variables.ts
+++ 3-typescript/features/variables.ts
@@ -39,4 +39,27 @@ const logNumber: (i: number) => void = (i: number) => {
 // 1) function that returns the 'any' type
 
 const json = '{"x": 10, "y": 20}';
-const coordinates = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+// 2) when we declare a variable on one line
+// and initialize it later
+
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  }
+}
+
+// 3) variable whos type cannot be inferred correctly
+
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
