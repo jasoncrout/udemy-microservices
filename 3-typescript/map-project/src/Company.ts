5be96b81b15888729d032e1c0c5a081182ea7f31
Jason Crout <croutjason@gmail.com>
2022-07-09 16:49:30 +1000

    feat: complete map project

--- 3-typescript/map-project/src/Company.ts
+++ 3-typescript/map-project/src/Company.ts
@@ -1,15 +1,17 @@
 import { faker } from "@faker-js/faker";
 import { Mappable } from "./CustomMap";
 
-export class User implements Mappable {
-  name: string;
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
   location: {
     lat: number;
     lng: number;
   };
 
   constructor() {
-    this.name = faker.name.firstName();
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
@@ -19,7 +21,8 @@ export class User implements Mappable {
   markerContent(): string {
     return `
     <div>
-    <h1>User Name: ${this.name}</h1>
+    <h1>Company Name: ${this.companyName}</h1>
+    <h3>Catchphrase: ${this.catchPhrase}</h3>
     </div>
     `;
   }
