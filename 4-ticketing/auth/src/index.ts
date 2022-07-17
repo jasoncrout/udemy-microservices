5a00086791cd002fe03c695f0fc7ddcf8bcf4649
Jason Crout <croutjason@gmail.com>
2022-07-17 13:36:26 +1000

    feat: add reusable error checking

--- 4-ticketing/auth/src/index.ts
+++ 4-ticketing/auth/src/index.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import "express-async-errors";
+
+import { currentUserRouter } from "./routes/currentuser";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/singup";
+import { errorHandler } from "./middlewares/error-handler";
+import { RouteNotFoundError } from "./errors/route-not-found-error";
 
 const app = express();
 app.use(express.json());
 
-app.route("/api/users/currentuser").get((req, res) => {
-  res.send("Hi there!");
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new RouteNotFoundError();
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
