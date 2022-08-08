3cb25de128516a499d9f2d607000510c3735d056
Jason Crout <croutjason@gmail.com>
2022-08-09 08:21:27 +1000

    feat: create userequest hook

--- 4-ticketing/client/pages/index.js
+++ 4-ticketing/client/pages/index.js
@@ -1,4 +1,18 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get("/api/users/currentuser").catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = () => {
+  console.log("I am on the server!");
+
+  return { color: "red" };
+};
+
 export default LandingPage;
