89b98a3800dfe379d117e79593ece971ef965ddd
Jason Crout <croutjason@gmail.com>
2022-08-26 21:31:08 +1000

    feat: add update ticket nats wrapper

--- 4-ticketing/tickets/src/events/publishers/ticket-created-publisher.ts
+++ 4-ticketing/tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,5 +1,4 @@
 import { Publisher, Subjects, TicketCreatedEvent } from "@jc-ticketing/common";
-import { Ticket } from "../../models/ticket";
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
--- 4-ticketing/tickets/src/routes/new.ts
+++ 4-ticketing/tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@jc-ticketing/common";
 import { Ticket } from "../models/ticket";
 import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -26,12 +27,12 @@ router.post(
     });
     await ticket.save();
 
-    // await new TicketCreatedPublisher().publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId,
-    // });
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   },
