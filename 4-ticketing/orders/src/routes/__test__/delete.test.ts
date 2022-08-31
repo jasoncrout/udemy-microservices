4a1068c1d01375a90b1831590d1a26c103abf475
Jason Crout <croutjason@gmail.com>
2022-09-01 08:08:14 +1000

    feat: completed publishing events for order service

--- 4-ticketing/orders/src/routes/__test__/delete.test.ts
+++ 4-ticketing/orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   // create a ticket with Ticket model
@@ -33,4 +34,29 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emites an order cancelled event");
+it("emites an order cancelled event", async () => {
+  // create a ticket with Ticket model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = signin();
+
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel an order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
