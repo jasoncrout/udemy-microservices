228257c58b212926c6a86a1d4483a6910efac917
Jason Crout <croutjason@gmail.com>
2022-09-11 16:08:53 +1000

    feat: add more order listener classes with relevant tests

--- 4-ticketing/tickets/src/events/publishers/ticket-created-publisher.ts
+++ 4-ticketing/tickets/src/events/publishers/ticket-created-publisher.ts
@@ -1,4 +1,5 @@
 import { Publisher, Subjects, TicketCreatedEvent } from "@jc-ticketing/common";
+import { TicketUpdatedPublisher } from "./ticket-updated-publisher";
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
   readonly subject = Subjects.TicketCreated;
