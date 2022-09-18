9d7a4acc492cd2983e65777d5e19ee066c5369bd
Jason Crout <croutjason@gmail.com>
2022-09-19 07:56:11 +1000

    feat: add stripe integration

--- 4-ticketing/payments/src/routes/new.ts
+++ 4-ticketing/payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from "@jc-ticketing/common";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -36,6 +37,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
+
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.status(201).send({ success: true });
   },
